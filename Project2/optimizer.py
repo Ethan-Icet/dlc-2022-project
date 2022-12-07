c4af4b49d44b375cb27dadae066ddf4355865bbb
Ethan Icet <ethanicet@gmail.com>
2022-12-08 00:39:06 +0100

    Implemented the SGD (using pytorch docs/pseudocode) so added optional momentum parameters

--- Project2/optimizer.py
+++ Project2/optimizer.py
@@ -15,7 +15,7 @@ class Optimizer:
         pass
 
 
-class SGD(Optimizer):
+class Basic(Optimizer):
     def __init__(self, parameters: list, lr: float) -> None:
         super().__init__()
         self.parameters = parameters
@@ -30,4 +30,57 @@ class SGD(Optimizer):
             param -= self.lr * grad
 
     def __repr__(self) -> str:
-        return f"SGD(lr={self.lr})"
+        return f"Basic(lr={self.lr})"
+
+
+class SGD(Optimizer):
+
+    def __init__(self, parameters: list, lr: float,
+                 momentum: float = 0.0, weight_decay: float = 0.0, dampening: float = 0.0,
+                 *, nesterov: bool = False, maximize: bool = False) -> None:
+        super().__init__()
+        self.parameters = parameters
+        self.lr = lr
+        self.momentum = momentum
+        self.weight_decay = weight_decay
+        self.dampening = dampening
+        self.nesterov = nesterov
+        self.maximize = maximize
+        self.v = [torch.zeros_like(param) for param, _ in self.parameters]  # momentum buffer
+        self.t = 0  # number of steps
+
+    def zero_grad(self) -> None:
+        for _, g in self.parameters:
+            g.zero_()
+
+    def step(self) -> None:
+        # Based on the pseudocode from https://pytorch.org/docs/stable/generated/torch.optim.SGD.html
+        for i, (param, grad) in enumerate(self.parameters):
+            if self.weight_decay != 0:
+                grad += self.weight_decay * param
+            if self.momentum != 0:
+                if self.t > 0:
+                    self.v[i] = self.momentum * self.v[i] + (1 - self.dampening) * grad
+                else:
+                    self.v[i] = grad
+                if self.nesterov:
+                    grad = grad + self.momentum * self.v[i]
+                else:
+                    grad = self.v[i]
+            if self.maximize:
+                param += self.lr * grad
+            else:
+                param -= self.lr * grad
+
+    def __repr__(self) -> str:
+        s = f"SGD(lr={self.lr}"
+        if self.momentum != 0:
+            s += f", momentum={self.momentum}"
+        if self.weight_decay != 0:
+            s += f", weight_decay={self.weight_decay}"
+        if self.dampening != 0:
+            s += f", dampening={self.dampening}"
+        if self.nesterov:
+            s += ", nesterov=True"
+        s += ")"
+        return s
