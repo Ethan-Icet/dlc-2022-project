39fb0128c3c952218e09c0bc204d64f137a2b04f
kkf17 <favre.kathleen@orange.fr>
2022-12-16 18:51:08 +0100

    updt2

--- Project2/module.py
+++
@@ -1,9 +0,0 @@
-class Module(object):
-    def forward(self, *inputs):
-        raise NotImplementedError
-
-    def backward(self, *grads_wrt_output):
-        raise NotImplementedError
-
-    def param(self) -> list:
-        return []
